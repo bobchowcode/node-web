f1d4e4edcda99a223439059a1972b4ec50b64650
bob chow <>
2018-04-01 13:49:41 +0800

    no message

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
