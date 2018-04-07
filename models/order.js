6a104885aa414306934a1964e801423262492105
bob chow <>
2018-04-08 00:41:08 +0800

    update schema

--- models/order.js
+++ models/order.js
@@ -1,12 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 var schema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
-    cart: {type: Object, required: true},
-    address: {type: String, required: true},
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    cart: { type: Object, required: true },
+    address: { type: String, required: true },
     // name: {type: String, required: true},
-    ccNo: {type: String, required: true}
+    ccNo: { type: String, required: true }
 });
+
+schema.methods.encryptCCNo = function (ccNo) {
+    return bcrypt.hashSync(ccNo, bcrypt.genSaltSync(5), null);
+};
+
+schema.methods.validCCNo = function (ccNo) {
+    return bcrypt.compareSync(ccNo, this.ccNo);
+};
 
 module.exports = mongoose.model('Order', schema);
--- models/user.js
+++ models/user.js
@@ -2,19 +2,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
-var userSchema = new Schema({
+var schema = new Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
     role: {type: String, required: true}
 });
 
-userSchema.methods.encryptPassword = function(password) {
+schema.methods.encryptPassword = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);  
 };
 
-userSchema.methods.validPassword = function(password) {
+schema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.password);  
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', schema);
