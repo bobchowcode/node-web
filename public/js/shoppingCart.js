895907b8f3da31545e68d8de59a507de325a85ce
polyu-17002407g <polyu17002407g@gmail.com>
2018-04-14 14:08:58 +0800

    shopping cart total cost

--- public/js/shoppingCart.js
+++ public/js/shoppingCart.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var totalCost = 0;
+
     $.ajax({
         url: "/product/getCartList",
         contentType: 'application/json; charset=utf-8',
@@ -10,6 +12,7 @@ $(document).ready(function () {
         }
     }).always(function() {
         $("#loading-panel").fadeOut("fast");
+        $("#cartTotalCost").html("HK" + currency(totalCost, { formatWithSymbol: true }).format());
     });
 
     function createProductRow(i, data) {
@@ -24,31 +27,26 @@ $(document).ready(function () {
         var product_button_div = $(product_clone).find("#temp_product_button");
         var product_button = $(product_button_div).find("button");
         var product_id = "product_" + idx;
+        var product_cost = currency(product.price*product.quantity);
+        totalCost = currency(totalCost).add(product_cost);
 
         $(product_clone).attr("id", product_id + "_row");
         $(product_title).attr("id", product_id + "_title");
         $(product_price).attr("id", product_id + "_price");
         $(product_button_div).attr("id", product_id + "_button");
 
-        $(product_title).html(data.title);
-        $(product_quantity_span).html(data.quantity);
-        $(product_price).html("HK" + currency(data.price*data.quantity, { formatWithSymbol: true }).format());
+        $(product_title).html(product.title);
+        $(product_quantity_span).html(product.quantity);
+        $(product_price).html("HK" + currency(product_cost, { formatWithSymbol: true }).format());
         $(product_button).on("click", function () {
-            minusProduct(product);
-            product.quantity--;
-            $(product_quantity_span).html(data.quantity);
-            if (product.quantity <= 0) {
-                $(product_clone).slideUp("slow", function() {
-                    $(product_clone).remove();
-                });
-            }
+            minusProduct(product, product_clone, product_quantity_span);
         });
         $(product_clone).hide();
         $(product_list).append(product_clone);
         $(product_clone).fadeIn("slow");
     }
 
-    function minusProduct(product) {
+    function minusProduct(product, product_elem, product_elem_quantity) {
         $("#loading-panel").fadeIn("fast");
         $.ajax({
             url: "/product/minusProduct",
@@ -57,7 +55,17 @@ $(document).ready(function () {
             dataType: "json",
             data: JSON.stringify(product),
             success: function (data) {
-               
+               if (data.res === "success") {
+                   product.quantity--;
+                   $(product_elem_quantity).html(product.quantity);
+                   if (product.quantity <= 0) {
+                       $(product_elem).slideUp("slow", function() {
+                           $(product_elem).remove();
+                        });
+                    }
+                    totalCost = currency(totalCost).subtract(product.price);
+                    $("#cartTotalCost").html("HK" + currency(totalCost, { formatWithSymbol: true }).format());
+               }
             }
         }).always(function() {
             $("#loading-panel").fadeOut("fast");
